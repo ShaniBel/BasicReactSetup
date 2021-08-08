997bdffa24ed2667ea8dadf1cf1e230ea9a84f7b
Shani B <shanibelisha2@gmail.com>
2021-08-08 09:57:14 +0300

    adding public dir

--- basic_react_setup/src/index.js
+++ basic_react_setup/src/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css'
+import "./index.css";
 
-// import HelloEl from './HelloEl'
+// import HelloEl from "./HelloEl";
 
+const helloEl = <div className="hello">Hello World!!</div>;
 
-const helloEl = <div className="hello">Hello World!!</div>
-
-
-ReactDOM.render(helloEl, document.querySelector("#root"))
+ReactDOM.render(helloEl, document.querySelector("#root"));
